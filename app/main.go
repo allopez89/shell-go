318985448b4c998429844c7bdc7f115b6ae6c30e
Axel López <axellpz89@hotmail.com>
2026-02-15 20:20:38 -0400

    Receive user input

--- app/main.go
+++ app/main.go
@@ -6,5 +6,9 @@ import (
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
+	var command string
+
 	fmt.Print("$ ")
+	fmt.Scanf("%v", &command)
+	fmt.Print(command, ": command not found")
 }
