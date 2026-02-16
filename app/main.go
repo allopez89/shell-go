3944ca3f181c6eeb7f0de395a8143bec090f1849
Axel López <axellpz89@hotmail.com>
2026-02-15 20:39:24 -0400

    Implement REPL

--- app/main.go
+++ app/main.go
@@ -4,17 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 
-	fmt.Print("$ ")
+	for {
+		fmt.Print("$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+
+		command = strings.TrimSpace(command)
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
