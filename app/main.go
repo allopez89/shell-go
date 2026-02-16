369ee21a7970e6fa4037c6086278639d70fa563f
Axel López <axellpz89@hotmail.com>
2026-02-15 22:07:28 -0400

    refactor, implemented echo command

--- app/main.go
+++ app/main.go
@@ -10,20 +10,32 @@ import (
 func main() {
 
 	for {
+
+		allowed_commands := map[string]func(args []string){
+			"exit": func(args []string) { os.Exit(0) },
+			"echo": func(args []string) {
+				fmt.Println(strings.Join(args, " "))
+			},
+		}
+
 		fmt.Print("$ ")
 
-		// Wait for user input
+		// Wait for user inputs
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
+		input := strings.TrimSpace(command)
+		parts := strings.Fields(input)
 
-		command = strings.TrimSpace(command)
-		if command == "exit" {
-			os.Exit(0)
+		input_command := parts[0]
+		input_arguments := parts[1:]
+
+		if command_func, ok := allowed_commands[input_command]; ok {
+			command_func(input_arguments)
+		} else {
+			fmt.Println(input_command + ": command not found")
 		}
-
-		fmt.Println(command + ": command not found")
 	}
 }
